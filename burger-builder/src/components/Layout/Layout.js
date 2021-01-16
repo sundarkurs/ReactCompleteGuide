e48dba0d52c748b95c5a5f9b6d9a92e72fe5323e
Sundar Urs <sundarkurs@gmail.com>
2021-01-16 11:07:01 +0530

    BB - Layout structure

---
+++ burger-builder/src/components/Layout/Layout.js
@@ -0,0 +1,11 @@
+import React from "react";
+import Auxiliary from "../../hoc/Auxiliary/Auxiliary";
+
+const layout = (props) => (
+  <Auxiliary>
+    <div>Toolbar, Sidebar, Backdrop</div>
+    <main>{props.children}</main>
+  </Auxiliary>
+);
+
+export default layout;
