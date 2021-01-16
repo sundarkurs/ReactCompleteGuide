1e492402e3901030ce8ff2a660afc4903baf6dcb
Sundar Urs <sundarkurs@gmail.com>
2021-01-16 20:21:21 +0530

    logo added

--- burger-builder/src/components/Layout/Layout.js
+++ burger-builder/src/components/Layout/Layout.js
@@ -1,10 +1,11 @@
 import React from "react";
 import Auxiliary from "../../hoc/Auxiliary/Auxiliary";
+import Toolbar from "../Navigation/Toolbar/Toolbar";
 import classes from "./Layout.module.css";
 
 const layout = (props) => (
   <Auxiliary>
-    <div>Toolbar, Sidebar, Backdrop</div>
+    <Toolbar></Toolbar>
     <main className={classes.Content}>{props.children}</main>
   </Auxiliary>
 );
