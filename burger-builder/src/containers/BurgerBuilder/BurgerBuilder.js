3dee67d613ea53da5107b9b0a5c74fa843d7ccf2
Sundar Urs <sundarkurs@gmail.com>
2021-01-16 11:59:06 +0530

    Created a burger

--- burger-builder/src/containers/BurgerBuilder/BurgerBuilder.js
+++ burger-builder/src/containers/BurgerBuilder/BurgerBuilder.js
@@ -1,12 +1,12 @@
 import React, { Component } from "react";
+import Burger from "../../components/Burger/Burger";
 import Auxiliary from "../../hoc/Auxiliary/Auxiliary";
 
 class BurgerBuilder extends Component {
   render() {
     return (
       <Auxiliary>
-        <div>Burger</div>
-        <div>Build Controls</div>
+        <Burger></Burger>
       </Auxiliary>
     );
   }
