e9adf19865f1d10459c90a01d24ea4b5b77a0514
Sundar Urs <sundarkurs@gmail.com>
2021-01-17 17:29:30 +0530

    minor folder structure change

--- burger-builder/src/containers/Layout/Layout.js
+++ burger-builder/src/containers/Layout/Layout.js
@@ -1,7 +1,7 @@
 import React, { Component } from "react";
 import Auxiliary from "../../hoc/Auxiliary/Auxiliary";
-import SideDrawer from "../Navigation/SideDrawer/SideDrawer";
-import Toolbar from "../Navigation/Toolbar/Toolbar";
+import SideDrawer from "../../components/Navigation/SideDrawer/SideDrawer";
+import Toolbar from "../../components/Navigation/Toolbar/Toolbar";
 import classes from "./Layout.module.css";
 
 class Layout extends Component {
