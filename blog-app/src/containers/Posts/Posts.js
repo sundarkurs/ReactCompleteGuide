9704ebf9e9105f52c6a7c3768609c4bd8c861f92
Sundar Urs <sundarkurs@gmail.com>
2021-01-23 19:24:21 +0530

    some restructuring to start the routing

---
+++ blog-app/src/containers/Posts/Posts.js
@@ -0,0 +1,48 @@
+import React, { Component } from 'react';
+import Post from '../../components/Post/Post';
+import './Posts.css';
+import axios from '../../axios';
+
+class Posts extends Component {
+    state = {
+        posts: [],
+    };
+
+    postSelectedHandler = (id) => {
+        this.setState({ selectedPostId: id });
+    };
+
+    componentDidMount() {
+        axios.get("/posts")
+            .then(response => {
+                const posts = response.data.slice(0, 4);
+                const updatedPosts = posts.map(post => {
+                    return { ...post, author: "Sundar" };
+                });
+
+                this.setState({ posts: updatedPosts });
+            })
+            .catch(error => {
+                //this.setState({ error: true })
+            })
+    }
+
+    render() {
+        let postItems = <p style={{ textAlign: 'center' }}>Something went wrong....</p>;
+
+        if (!this.state.error) {
+            postItems = this.state.posts.map((post, index) => {
+                return <Post key={post.id} post={post} clicked={() => this.postSelectedHandler(post.id)}></Post>
+            });
+        }
+
+        return (
+            <section className="Posts">
+                {postItems}
+            </section>
+        );
+
+    }
+}
+
+export default Posts;
