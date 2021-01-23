355b4bb3da65b808a406522e9a8c272d10013c11
Sundar Urs <sundarkurs@gmail.com>
2021-01-23 13:33:53 +0530

    axios defaults

--- ajax-app/src/index.js
+++ ajax-app/src/index.js
@@ -5,9 +5,12 @@ import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 import axios from 'axios';
 
+axios.defaults.baseURL = "https://jsonplaceholder.typicode.com";
+axios.defaults.headers.common['Authorization'] = 'Bearer ABC';
+axios.defaults.headers.post['Content-Type'] = "application/json";
+
 axios.interceptors.request.use(request => {
     console.log(request);
-
     return request;
 }, error => {
     console.log(error);
@@ -23,5 +26,5 @@ axios.interceptors.response.use(response => {
     return Promise.reject(error);
 });
 
-ReactDOM.render( <App />, document.getElementById( 'root' ) );
+ReactDOM.render(<App />, document.getElementById('root'));
 registerServiceWorker();
