2a8d80a167f148d79cf6f4fbadf4763794d136e9
Sundar Urs <sundarkurs@gmail.com>
2021-01-30 13:53:52 +0530

    added order summary component

--- burger-builder/src/containers/Checkout/Checkout.js
+++ burger-builder/src/containers/Checkout/Checkout.js
@@ -1,5 +1,7 @@
 import React, { Component } from 'react';
+import { Route } from "react-router-dom";
 import CheckoutSummary from '../../components/Order/CheckoutSummary/CheckoutSummary';
+import ContactData from './ContactData/ContactData';
 
 class Checkout extends Component {
 
@@ -32,11 +34,15 @@ class Checkout extends Component {
     }
 
     render() {
+        console.log(this.props);
+
         return (
             <div>
                 <CheckoutSummary ingredients={this.state.ingredients}
                     checkoutCancelled={this.checkoutCancelledHandler}
                     checkoutContinued={this.checkoutContinuedHandler}></CheckoutSummary>
+
+                <Route exact path={this.props.match.path + '/contact-data'} component={ContactData} />
             </div>
         );
     }
